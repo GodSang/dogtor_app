188587bd9fc5a22f079341d5d0d82f3c9bdd4a50
GodSang <tkdgh584@gmail.com>
2021-05-12 16:45:54 +0900

    weight ViewPager 및 Indicator 구현

--- app/src/main/java/com/example/capstone_design/FragmentWeight.java
+++ app/src/main/java/com/example/capstone_design/FragmentWeight.java
@@ -1,21 +1,42 @@
 package com.example.capstone_design;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.capstone_design.recyclerview.ViewPagerAdapter;
+
+import me.relex.circleindicator.CircleIndicator;
+
 public class FragmentWeight extends Fragment {
+
+    Activity activity;
+    Context context;
+    ViewPagerAdapter adapter;
+    ViewPager viewPager;
+    CircleIndicator indicator;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_weight, container, false);
+        View view = inflater.inflate(R.layout.fragment_weight, container, false);
+        activity = getActivity();
+        context = getContext();
 
-        return v;
+        viewPager = viewPager.findViewById(R.id.pager);
+        adapter = new ViewPagerAdapter(context);
+        viewPager.setAdapter(adapter);
+        indicator = view.findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/capstone_design/recyclerview/ViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.capstone_design.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.capstone_design.R;
+
+public class ViewPagerAdapter extends PagerAdapter{
+    private LayoutInflater inflater;
+    private Context context;
+    private int[] images = {R.drawable.dog_profile_1, R.drawable.dog_profile_2, R.drawable.dog_profile_3};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((View)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_weight_image, container, false);
+        ImageView imageView = view.findViewById(R.id.vp_iv_image);
+        imageView.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.invalidate();
+    }
+}
